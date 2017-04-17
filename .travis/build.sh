06a8ac49d06b1fbe5148af5daeed62e9ded2a1ae
Hadrien Mary <hadrien.mary@gmail.com>
2017-04-17 14:49:35 -0400

    Update Travis script

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,14 +1,35 @@
 #!/bin/sh
 set -e
 
-dir="$(dirname "$0")"
 test "$TRAVIS_PULL_REQUEST" = false \
      -a "$TRAVIS_BRANCH" = master
 
 # Build and install Beakerx base kernel (no artifact available for now)
+cd $HOME/
 git clone --depth 1 https://github.com/twosigma/beakerx.git
 cd beakerx/kernel/base
 gradle publishToMavenLocal
-cd ../../../
 
-mvn install
+# Define some variables
+export USER="Scijava-jupyter-kernel"
+export UPDATE_SITE="Scijava-jupyter-kernel"
+
+export IJ_PATH="$HOME/Fiji.app"
+export URL="http://sites.imagej.net/$UPDATE_SITE/"
+export IJ_LAUNCHER="$IJ_PATH/ImageJ-linux64"
+export PATH="$IJ_PATH:$PATH"
+
+# Install ImageJ
+mkdir -p $IJ_PATH/
+cd $HOME/
+wget --no-check-certificate https://downloads.imagej.net/fiji/latest/fiji-linux64.zip
+unzip fiji-linux64.zip
+
+# Install the package
+cd $TRAVIS_BUILD_DIR/
+mvn install -Pimagej --settings ".travis/settings.xml"
+
+# Deploy the package
+$IJ_LAUNCHER --update edit-update-site $UPDATE_SITE $URL "webdav:$USER:$WIKI_UPLOAD_PASS" .
+$IJ_LAUNCHER --update upload --update-site $UPDATE_SITE --force-shadow jars/beaker-kernel-base.jar
+$IJ_LAUNCHER --update upload --update-site $UPDATE_SITE --force-shadow jars/scijava-jupyter-kernel.jar
