360aaf3fbb54063ac06938b083e99360385e5a20
Hadrien Mary <hadrien.mary@gmail.com>
2017-04-17 11:33:40 -0400

    Add Groovy test file for developing

--- src/test/java/org/scijava/jupyterkernel/TestGroovy.java
+++ src/test/java/org/scijava/jupyterkernel/TestGroovy.java
@@ -8,16 +8,11 @@ package org.scijava.jupyterkernel;
 import java.io.Reader;
 import java.io.StringReader;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.script.ScriptEngine;
 import javax.script.ScriptException;
 import net.imagej.ImageJ;
 import net.imagej.Main;
 import org.scijava.module.ModuleItem;
-import org.scijava.module.process.ModulePreprocessor;
-import org.scijava.module.process.PostprocessorPlugin;
 import org.scijava.module.process.PreprocessorPlugin;
 import org.scijava.script.ScriptInfo;
 import org.scijava.script.ScriptLanguage;
@@ -27,21 +22,23 @@ import org.scijava.script.ScriptModule;
  *
  * @author Hadrien Mary
  */
-public class TestAnnotations {
+public class TestGroovy {
 
-    public static void main(final String[] args) throws ScriptException {
+    public static void main(String[] args) throws ScriptException {
         ImageJ ij = Main.launch(args);
 
-        String code = "#@LogService log\n#@ImageJ ij\nprint('log')\nprint('jjjj')";
+        String code = "";
+        //code += "@Grab('org.springframework:spring-orm:3.2.5.RELEASE')\n";
+        code += "import org.springframework.jdbc.core.JdbcTemplate\nprintln JdbcTemplate\n";
+        code += "println 'test'";
         final Reader input = new StringReader(code);
 
         ScriptInfo info = new ScriptInfo(ij.context(), "dummy.py", input);
         final String path = info.getPath();
 
         List<? extends PreprocessorPlugin> pre = ij.plugin().createInstancesOfType(PreprocessorPlugin.class);
-        List<? extends PostprocessorPlugin> post = ij.plugin().createInstancesOfType(PostprocessorPlugin.class);
 
-        ScriptLanguage scriptLanguage = ij.script().getLanguageByName("jython");
+        ScriptLanguage scriptLanguage = ij.script().getLanguageByName("groovy");
         ScriptEngine scriptEngine = scriptLanguage.getScriptEngine();
 
         ScriptModule module;
@@ -49,23 +46,19 @@ public class TestAnnotations {
         ij.context().inject(module);
         module.setLanguage(scriptLanguage);
 
-        for (final ModulePreprocessor p : pre) {
+        pre.forEach((p) -> {
             p.process(module);
-        }
-
-        ij.log().info(module);
-        ij.log().info(module.getInputs());
+        });
         
         for (final ModuleItem<?> item : info.inputs()) {
             final String name = item.getName();
-            ij.log().info(name);
             scriptEngine.put(name, module.getInput(name));
         }
         
         scriptEngine.eval(code);
         
-        //ij.module().run(module, true);
-
+        ij.context().dispose();
+        
     }
 
 }
