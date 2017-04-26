af2cad953a65c1ea0d234efdcb8129db1608c1ab
Hadrien Mary <hadrien.mary@gmail.com>
2017-04-26 19:25:26 -0400

    Fix travis test

--- .travis/test.sh
+++ .travis/test.sh
@@ -15,7 +15,7 @@ conda update --yes conda
 conda update --yes --all
 conda info -a
 
-conda create --yes -n scijava python jupyter 
+conda create --yes -n scijava python jupyter
 
 source activate scijava
 
@@ -33,5 +33,17 @@ jupyter kernelspec list
 
 # Now run some notebooks
 
-python "$TRAVIS_BUILD_DIR/scripts/runipy.py" "$TRAVIS_BUILD_DIR/notebooks/Python.ipynb" --kernel_name "scijava-python"
-python "$TRAVIS_BUILD_DIR/scripts/runipy.py" "$TRAVIS_BUILD_DIR/notebooks/Groovy.ipynb" --kernel_name "scijava-groovy"
+python "$TRAVIS_BUILD_DIR/scripts/runipy.py" "$TRAVIS_BUILD_DIR/notebooks/Welcome.ipynb" --kernel_name "scijava-python"
+python "$TRAVIS_BUILD_DIR/scripts/runipy.py" "$TRAVIS_BUILD_DIR/notebooks/General.ipynb" --kernel_name "scijava-groovy"
+python "$TRAVIS_BUILD_DIR/scripts/runipy.py" "$TRAVIS_BUILD_DIR/notebooks/Rich\ Output.ipynb" --kernel_name "scijava-python"
+python "$TRAVIS_BUILD_DIR/scripts/runipy.py" "$TRAVIS_BUILD_DIR/notebooks/On-The-Fly\ Grabbing.ipynb" --kernel_name "scijava-python"
+python "$TRAVIS_BUILD_DIR/scripts/runipy.py" "$TRAVIS_BUILD_DIR/notebooks/ImageJ.ipynb" --kernel_name "scijava-groovy"
+python "$TRAVIS_BUILD_DIR/scripts/runipy.py" "$TRAVIS_BUILD_DIR/notebooks/Scijava.ipynb" --kernel_name "scijava-groovy"
+
+python "$TRAVIS_BUILD_DIR/scripts/runipy.py" "$TRAVIS_BUILD_DIR/notebooks/languages/Python.ipynb" --kernel_name "scijava-python"
+python "$TRAVIS_BUILD_DIR/scripts/runipy.py" "$TRAVIS_BUILD_DIR/notebooks/languages/Groovy.ipynb" --kernel_name "scijava-groovy"
+python "$TRAVIS_BUILD_DIR/scripts/runipy.py" "$TRAVIS_BUILD_DIR/notebooks/languages/Scala.ipynb" --kernel_name "scijava-scala"
+python "$TRAVIS_BUILD_DIR/scripts/runipy.py" "$TRAVIS_BUILD_DIR/notebooks/languages/Clojure.ipynb" --kernel_name "scijava-clojure"
+python "$TRAVIS_BUILD_DIR/scripts/runipy.py" "$TRAVIS_BUILD_DIR/notebooks/languages/Beanshell.ipynb" --kernel_name "scijava-beanshell"
+python "$TRAVIS_BUILD_DIR/scripts/runipy.py" "$TRAVIS_BUILD_DIR/notebooks/languages/Javascript.ipynb" --kernel_name "scijava-javascript"
+
