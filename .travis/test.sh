58a47aac411cc3880fecc52459276fc829740cf6
Hadrien Mary <hadrien.mary@gmail.com>
2017-05-02 12:39:27 -0400

    Travis again

--- .travis/test.sh
+++ .travis/test.sh
@@ -15,7 +15,7 @@ conda update --yes conda
 conda update --yes --all
 conda info -a
 
-conda create --yes -n scijava python jupyter
+conda create --yes -n scijava python jupyter nbconvert
 
 source activate scijava
 
@@ -34,17 +34,17 @@ jupyter kernelspec list
 # Now run some notebooks
 NB_COMMAND="jupyter nbconvert --execute --ExecutePreprocessor.timeout=60"
 
-"$NB_COMMAND $TRAVIS_BUILD_DIR/notebooks/Welcome.ipynb --ExecutePreprocessor.kernel_name=scijava-python"
-"$NB_COMMAND $TRAVIS_BUILD_DIR/notebooks/General.ipynb --ExecutePreprocessor.kernel_name=scijava-groovy"
-"$NB_COMMAND $TRAVIS_BUILD_DIR/notebooks/Rich Output.ipynb --ExecutePreprocessor.kernel_name=scijava-python"
-"$NB_COMMAND $TRAVIS_BUILD_DIR/notebooks/On-The-Fly Grabbing.ipynb --ExecutePreprocessor.kernel_name=scijava-python"
-"$NB_COMMAND $TRAVIS_BUILD_DIR/notebooks/ImageJ.ipynb --ExecutePreprocessor.kernel_name=scijava-groovy"
-"$NB_COMMAND $TRAVIS_BUILD_DIR/notebooks/Scijava.ipynb --ExecutePreprocessor.kernel_name=scijava-groovy"
+"$NB_COMMAND --ExecutePreprocessor.kernel_name=scijava-python $TRAVIS_BUILD_DIR/notebooks/Welcome.ipynb"
+"$NB_COMMAND --ExecutePreprocessor.kernel_name=scijava-groovy $TRAVIS_BUILD_DIR/notebooks/General.ipynb"
+"$NB_COMMAND --ExecutePreprocessor.kernel_name=scijava-python $TRAVIS_BUILD_DIR/notebooks/Rich Output.ipynb"
+"$NB_COMMAND --ExecutePreprocessor.kernel_name=scijava-python $TRAVIS_BUILD_DIR/notebooks/On-The-Fly Grabbing.ipynb"
+"$NB_COMMAND --ExecutePreprocessor.kernel_name=scijava-groovy $TRAVIS_BUILD_DIR/notebooks/ImageJ.ipynb"
+"$NB_COMMAND --ExecutePreprocessor.kernel_name=scijava-groovy $TRAVIS_BUILD_DIR/notebooks/Scijava.ipynb"
 
-"$NB_COMMAND $TRAVIS_BUILD_DIR/notebooks/languages/Python.ipynb --ExecutePreprocessor.kernel_name=scijava-python"
-"$NB_COMMAND $TRAVIS_BUILD_DIR/notebooks/languages/Groovy.ipynb --ExecutePreprocessor.kernel_name=scijava-groovy"
-"$NB_COMMAND $TRAVIS_BUILD_DIR/notebooks/languages/Scala.ipynb --ExecutePreprocessor.kernel_name=scijava-scala"
-"$NB_COMMAND $TRAVIS_BUILD_DIR/notebooks/languages/Clojure.ipynb --ExecutePreprocessor.kernel_name=scijava-clojure"
-"$NB_COMMAND $TRAVIS_BUILD_DIR/notebooks/languages/Beanshell.ipynb --ExecutePreprocessor.kernel_name=scijava-beanshell"
-"$NB_COMMAND $TRAVIS_BUILD_DIR/notebooks/languages/Javascript.ipynb --ExecutePreprocessor.kernel_name=scijava-javascript"
+"$NB_COMMAND --ExecutePreprocessor.kernel_name=scijava-python $TRAVIS_BUILD_DIR/notebooks/languages/Python.ipynb"
+"$NB_COMMAND --ExecutePreprocessor.kernel_name=scijava-groovy $TRAVIS_BUILD_DIR/notebooks/languages/Groovy.ipynb"
+"$NB_COMMAND --ExecutePreprocessor.kernel_name=scijava-scala $TRAVIS_BUILD_DIR/notebooks/languages/Scala.ipynb"
+"$NB_COMMAND --ExecutePreprocessor.kernel_name=scijava-clojure $TRAVIS_BUILD_DIR/notebooks/languages/Clojure.ipynb"
+"$NB_COMMAND --ExecutePreprocessor.kernel_name=scijava-beanshell $TRAVIS_BUILD_DIR/notebooks/languages/Beanshell.ipynb"
+"$NB_COMMAND --ExecutePreprocessor.kernel_name=scijava-javascript $TRAVIS_BUILD_DIR/notebooks/languages/Javascript.ipynb"
 
