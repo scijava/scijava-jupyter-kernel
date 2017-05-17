3ef0ab46798aec1ab8f14d5536490f13f9562dd9
Hadrien Mary <hadrien.mary@gmail.com>
2017-05-17 19:17:07 -0400

    Add TableToHTMLNotebookConverter

---
+++ src/main/java/net/imagej/notebook/converter/TableToHTMLNotebookConverter.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2017 SciJava.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.imagej.notebook.converter;
+
+import net.imagej.table.Table;
+import org.scijava.Priority;
+import org.scijava.convert.Converter;
+import org.scijava.notebook.converter.NotebookOutputConverter;
+import org.scijava.notebook.converter.ouput.HTMLNotebookOutput;
+import org.scijava.plugin.Plugin;
+
+@Plugin(type = Converter.class, priority = Priority.LOW_PRIORITY)
+public class TableToHTMLNotebookConverter<O extends Table>
+        extends NotebookOutputConverter<O, HTMLNotebookOutput> {
+
+    @Override
+    public Class getInputType() {
+        return Table.class;
+    }
+
+    @Override
+    public Class getOutputType() {
+        return HTMLNotebookOutput.class;
+    }
+
+    @Override
+    public HTMLNotebookOutput convert(Object object) {
+
+        Table table = (Table) object;
+
+        String htmlString = "<table>";
+
+        // Set column headers
+        htmlString += "<tr>";
+        for (int i = 0; i < table.getColumnCount(); i++) {
+            htmlString += "<th>";
+            htmlString += table.getColumnHeader(i);
+            htmlString += "</th>";
+        }
+        htmlString += "</tr>";
+
+        // Append the rows
+        for (int i = 0; i < table.getRowCount(); i++) {
+            htmlString += "<tr>";
+            for (int j = 0; j < table.getColumnCount(); j++) {
+                htmlString += "<td>";
+                htmlString += table.get(j, i);
+                htmlString += "</td>";
+            }
+            htmlString += "</tr>";
+        }
+
+        htmlString += "</table>";
+
+        return new HTMLNotebookOutput(HTMLNotebookOutput.getMimeType(), htmlString);
+    }
+
+}
