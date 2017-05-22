e983dc922d1cc63fb54a26c3574bee8162a0e739
Alison Walter <awalter2@wisc.edu>
2017-05-22 12:09:25 -0500

    Update to use table style and extend HTMLNotebookOutputConverter

--- src/main/java/net/imagej/notebook/converter/TableToHTMLNotebookConverter.java
+++ src/main/java/net/imagej/notebook/converter/TableToHTMLNotebookConverter.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,31 +20,35 @@
 
 package net.imagej.notebook.converter;
 
+import net.imagej.table.Column;
 import net.imagej.table.Table;
 
 import org.scijava.convert.Converter;
-import org.scijava.notebook.converter.NotebookOutputConverter;
-import org.scijava.notebook.converter.output.HTMLNotebookOutput;
+import org.scijava.notebook.converter.HTMLNotebookOutputConverter;
+import org.scijava.notebook.converter.output.HTMLTableNotebookOutput;
 import org.scijava.plugin.Plugin;
 
 @Plugin(type = Converter.class)
-public class TableToHTMLNotebookConverter<O extends Table>
-        extends NotebookOutputConverter<O, HTMLNotebookOutput> {
+public class TableToHTMLNotebookConverter<C extends Column<? extends T>, T>
+    extends HTMLNotebookOutputConverter<Table<C, T>, HTMLTableNotebookOutput>
+{
 
     @Override
-    public Class getInputType() {
-        return Table.class;
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    public Class<Table<C, T>> getInputType() {
+        return (Class) Table.class;
     }
 
     @Override
-    public Class getOutputType() {
-        return HTMLNotebookOutput.class;
+    public Class<HTMLTableNotebookOutput> getOutputType() {
+        return HTMLTableNotebookOutput.class;
     }
 
     @Override
-    public HTMLNotebookOutput convert(Object object) {
+    public HTMLTableNotebookOutput convert(final Object object) {
 
-        Table table = (Table) object;
+        @SuppressWarnings("unchecked")
+        final Table<C, T> table = (Table<C, T>) object;
 
         String htmlString = "<table>";
 
@@ -62,7 +66,7 @@ public class TableToHTMLNotebookConverter<O extends Table>
             htmlString += "<tr>";
             for (int j = 0; j < table.getColumnCount(); j++) {
                 htmlString += "<td>";
-                htmlString += table.get(j, i);
+                htmlString += asHTML(table.get(j, i));
                 htmlString += "</td>";
             }
             htmlString += "</tr>";
@@ -70,7 +74,7 @@ public class TableToHTMLNotebookConverter<O extends Table>
 
         htmlString += "</table>";
 
-        return new HTMLNotebookOutput(htmlString);
+        return new HTMLTableNotebookOutput(htmlString);
     }
 
 }
