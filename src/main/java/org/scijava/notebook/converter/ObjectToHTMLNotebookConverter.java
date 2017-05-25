9be2c160c5656353e598a55ba69f02e63c86d6d2
Alison Walter <awalter2@wisc.edu>
2017-05-25 14:31:18 -0500

    Change <wbr> to zero width space
    <wbr> is filtered out by http://nbviewer.jupyter.org which again causes
    tables to overflow in the x. However, zero width space works.

--- src/main/java/org/scijava/notebook/converter/ObjectToHTMLNotebookConverter.java
+++ src/main/java/org/scijava/notebook/converter/ObjectToHTMLNotebookConverter.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -26,8 +26,9 @@ import org.scijava.notebook.converter.output.HTMLNotebookOutput;
 import org.scijava.plugin.Plugin;
 
 @Plugin(type = Converter.class)
-public class ObjectToHTMLNotebookConverter
-        extends NotebookOutputConverter<Object, HTMLNotebookOutput> {
+public class ObjectToHTMLNotebookConverter extends
+    NotebookOutputConverter<Object, HTMLNotebookOutput>
+{
 
     @Override
     public Class<Object> getInputType() {
@@ -40,10 +41,10 @@ public class ObjectToHTMLNotebookConverter
     }
 
     @Override
-    public HTMLNotebookOutput convert(Object object) {
+    public HTMLNotebookOutput convert(final Object object) {
         final String escaped = StringEscapeUtils.escapeHtml4(object.toString());
-        // Add in <wbr> tags before @, ., $, _, and #
-        final String wordBreaks = escaped.replaceAll("([@.$_#])", "<wbr>$1");
+        // Add in zero width space character (&#8203;) before @, ., $, _, and #
+        final String wordBreaks = escaped.replaceAll("([@.$_#])", "&#8203;$1");
         return new HTMLNotebookOutput(wordBreaks);
     }
 
