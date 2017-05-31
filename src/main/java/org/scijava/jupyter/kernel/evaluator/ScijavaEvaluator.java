dbd866a80b2571d6fb3011cbbe3e955e01cefe1e
Hadrien Mary <hadrien.mary@gmail.com>
2017-05-31 08:00:24 -0400

    Disable autocompletion when no completer is available.

--- src/main/java/org/scijava/jupyter/kernel/evaluator/ScijavaEvaluator.java
+++ src/main/java/org/scijava/jupyter/kernel/evaluator/ScijavaEvaluator.java
@@ -17,7 +17,6 @@
  * limitations under the License.
  * #L%
  */
-
 package org.scijava.jupyter.kernel.evaluator;
 
 import com.twosigma.beaker.autocomplete.AutocompleteResult;
@@ -73,134 +72,142 @@ public class ScijavaEvaluator implements Evaluator {
     protected String sessionId;
 
     public ScijavaEvaluator(Context context, String shellId, String sessionId) {
-        context.inject(this);
+	context.inject(this);
 
-        this.shellId = shellId;
-        this.sessionId = sessionId;
+	this.shellId = shellId;
+	this.sessionId = sessionId;
 
-        this.scriptEngines = new HashMap<>();
-        this.scriptLanguages = new HashMap<>();
-        
-        this.completers = new HashMap<>();
+	this.scriptEngines = new HashMap<>();
+	this.scriptLanguages = new HashMap<>();
 
-        this.languageName = DEFAULT_LANGUAGE;
+	this.completers = new HashMap<>();
+
+	this.languageName = DEFAULT_LANGUAGE;
     }
 
     @Override
     public void setShellOptions(KernelParameters kp) throws IOException {
-        log.debug("Set shell options : " + kp);
+	log.debug("Set shell options : " + kp);
     }
 
     @Override
     public AutocompleteResult autocomplete(String code, int index) {
-        
-        // TODO: we need to find a way the language related to the current cell.
-        // For now, we are just using the last used language.
-        AutoCompleter completer = this.completers.get(this.languageName);
-        ScriptEngine scriptEngine = this.scriptEngines.get(this.languageName);
 
-        AutoCompletionResult result = completer.autocomplete(code, index, scriptEngine);
+	// TODO: we need to find a way the language related to the current cell.
+	// For now, we are just using the last used language.
+	AutoCompleter completer = this.completers.get(this.languageName);
+	ScriptEngine scriptEngine = this.scriptEngines.get(this.languageName);
 
-        List<String> matches = (List<String>) result.getMatches();
-        int startIndex = (int) result.getStartIndex();
+	List<String> matches;
+	int startIndex;
+	if (completer != null) {
+	    AutoCompletionResult result = completer.autocomplete(code, index, scriptEngine);
 
-        return new AutocompleteResult(matches, startIndex);
+	    matches = (List<String>) result.getMatches();
+	    startIndex = (int) result.getStartIndex();
+
+	} else {
+	    matches = new ArrayList<>();
+	    startIndex = 0;
+	}
+
+	return new AutocompleteResult(matches, startIndex);
     }
 
     @Override
     public void killAllThreads() {
-        log.debug("Kill All Threads");
-        // Ugly and not working :-(
-        System.exit(0);
+	log.debug("Kill All Threads");
+	// Ugly and not working :-(
+	System.exit(0);
     }
 
     @Override
     public void startWorker() {
-        // Nothing to do
+	// Nothing to do
     }
 
     @Override
     public void evaluate(SimpleEvaluationObject seo, String code) {
 
-        code = this.setLanguage(code);
+	code = this.setLanguage(code);
 
-        Worker worker = new Worker(this.context, this.scriptEngines, this.scriptLanguages);
-        worker.setup(seo, code, this.languageName);
-        this.threadService.queue(getClass().getName(), worker);
+	Worker worker = new Worker(this.context, this.scriptEngines, this.scriptLanguages);
+	worker.setup(seo, code, this.languageName);
+	this.threadService.queue(getClass().getName(), worker);
     }
 
     @Override
     public void exit() {
-        log.debug("Exiting DefaultEvaluator");
-        // Ugly and not working :-(
-        System.exit(0);
+	log.debug("Exiting DefaultEvaluator");
+	// Ugly and not working :-(
+	System.exit(0);
     }
 
     private void addLanguage(String langName) {
 
-        if (scriptService.getLanguageByName(langName) == null) {
-            log.error("Script Language for '" + langName + "' not found.");
-            System.exit(1);
-        }
+	if (scriptService.getLanguageByName(langName) == null) {
+	    log.error("Script Language for '" + langName + "' not found.");
+	    System.exit(1);
+	}
 
-        if (!this.scriptLanguages.keySet().contains(langName)) {
+	if (!this.scriptLanguages.keySet().contains(langName)) {
 
-            Bindings bindings = null;
-            if (!this.scriptEngines.isEmpty()) {
-                String firstLanguage = this.scriptEngines.keySet().iterator().next();
-                bindings = this.scriptEngines.get(firstLanguage).getBindings(ScriptContext.ENGINE_SCOPE);
-            }
+	    Bindings bindings = null;
+	    if (!this.scriptEngines.isEmpty()) {
+		String firstLanguage = this.scriptEngines.keySet().iterator().next();
+		bindings = this.scriptEngines.get(firstLanguage).getBindings(ScriptContext.ENGINE_SCOPE);
+	    }
 
-            log.info("Script Language for '" + langName + "' found.");
-            ScriptLanguage scriptLanguage = scriptService.getLanguageByName(langName);
-            this.scriptLanguages.put(langName, scriptLanguage);
+	    log.info("Script Language for '" + langName + "' found.");
+	    ScriptLanguage scriptLanguage = scriptService.getLanguageByName(langName);
+	    this.scriptLanguages.put(langName, scriptLanguage);
 
-            ScriptEngine engine = this.scriptLanguages.get(langName).getScriptEngine();
-            this.scriptEngines.put(langName, engine);
-            
-            AutoCompleter completer = scriptLanguage.getAutoCompleter();
-            this.completers.put(languageName, completer);
+	    ScriptEngine engine = this.scriptLanguages.get(langName).getScriptEngine();
+	    this.scriptEngines.put(langName, engine);
 
-            // Not implemented yet
-            //engine.setBindings(this.bindings, ScriptContext.ENGINE_SCOPE);
-            if (bindings != null) {
-                this.initBindings(bindings, engine, scriptLanguage);
-            }
+	    AutoCompleter completer = scriptLanguage.getAutoCompleter();
+	    this.completers.put(languageName, completer);
 
-        }
+	    // Not implemented yet
+	    //engine.setBindings(this.bindings, ScriptContext.ENGINE_SCOPE);
+	    if (bindings != null) {
+		this.initBindings(bindings, engine, scriptLanguage);
+	    }
 
-        log.debug("Script Language found for '" + langName + "'");
+	}
+
+	log.debug("Script Language found for '" + langName + "'");
     }
 
     private String setLanguage(String code) {
 
-        if (code.startsWith("#!")) {
+	if (code.startsWith("#!")) {
 
-            // If code is composed of multiple lines
-            if (code.split("\n").length > 1) {
-                this.languageName = code.substring(2, code.indexOf("\n")).trim();
+	    // If code is composed of multiple lines
+	    if (code.split("\n").length > 1) {
+		this.languageName = code.substring(2, code.indexOf("\n")).trim();
 
-                // Return the code string without the first line
-                code = code.substring(code.indexOf("\n") + 1);
-            } // If only one line
-            else {
-                this.languageName = code.substring(2).trim();
+		// Return the code string without the first line
+		code = code.substring(code.indexOf("\n") + 1);
+	    } // If only one line
+	    else {
+		this.languageName = code.substring(2).trim();
 
-                code = "";
-            }
+		code = "";
+	    }
 
-        }
+	}
 
-        this.addLanguage(this.languageName);
-        return code;
+	this.addLanguage(this.languageName);
+	return code;
     }
 
     private void initBindings(Bindings bindings, ScriptEngine scriptEngine, ScriptLanguage scriptLanguage) {
 
-        Bindings currentBindings = scriptEngine.getBindings(ScriptContext.ENGINE_SCOPE);
-        bindings.keySet().forEach((String key) -> {
-            currentBindings.put(key, scriptLanguage.decode(bindings.get(key)));
-        });
+	Bindings currentBindings = scriptEngine.getBindings(ScriptContext.ENGINE_SCOPE);
+	bindings.keySet().forEach((String key) -> {
+	    currentBindings.put(key, scriptLanguage.decode(bindings.get(key)));
+	});
 
     }
 
