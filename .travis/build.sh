89f513a9d17e514b94021686eddb0ad4f80179f7
Hadrien Mary <hadrien.mary@gmail.com>
2017-06-02 17:53:11 -0400

    Add profile

--- .travis/build.sh
+++ .travis/build.sh
@@ -4,8 +4,8 @@ if [ "$TRAVIS_SECURE_ENV_VARS" = true \
   -a "$TRAVIS_PULL_REQUEST" = false \
   -a "$TRAVIS_BRANCH" = master ]
 then
-  mvn install --settings "$dir/settings.xml"
+  mvn install -Pimagej --settings "$dir/settings.xml"
   #mvn -Pdeploy-to-imagej deploy --settings "$dir/settings.xml"
 else
-  mvn install --settings "$dir/settings.xml"
+  mvn install -Pimagej --settings "$dir/settings.xml"
 fi
