31950928ebd52e14f0c664a6f016ed667cd4e551
Hadrien Mary <hadrien.mary@gmail.com>
2017-06-02 17:50:30 -0400

    Add setting file path to mvn command

--- .travis/build.sh
+++ .travis/build.sh
@@ -4,8 +4,8 @@ if [ "$TRAVIS_SECURE_ENV_VARS" = true \
   -a "$TRAVIS_PULL_REQUEST" = false \
   -a "$TRAVIS_BRANCH" = master ]
 then
-  mvn install
+  mvn install --settings "$dir/settings.xml"
   #mvn -Pdeploy-to-imagej deploy --settings "$dir/settings.xml"
 else
-  mvn install
+  mvn install --settings "$dir/settings.xml"
 fi
