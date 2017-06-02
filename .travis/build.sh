15f1362a30a54d402a695e6a4a1cba3a01f18663
Hadrien Mary <hadrien.mary@gmail.com>
2017-06-02 17:43:51 -0400

    Disable deploy for now

--- .travis/build.sh
+++ .travis/build.sh
@@ -4,7 +4,8 @@ if [ "$TRAVIS_SECURE_ENV_VARS" = true \
   -a "$TRAVIS_PULL_REQUEST" = false \
   -a "$TRAVIS_BRANCH" = master ]
 then
-  mvn -Pdeploy-to-imagej deploy --settings "$dir/settings.xml"
+  mvn install
+  #mvn -Pdeploy-to-imagej deploy --settings "$dir/settings.xml"
 else
   mvn install
 fi
