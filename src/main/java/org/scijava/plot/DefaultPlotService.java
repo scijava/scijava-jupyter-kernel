ba9cb9ab7e245555dadb3cfdc8d155db4a58b6b0
Hadrien Mary <hadrien.mary@gmail.com>
2017-07-29 17:46:18 -0400

    Add plot service

---
+++ src/main/java/org/scijava/plot/DefaultPlotService.java
@@ -0,0 +1,73 @@
+/*-
+ * #%L
+ * SciJava polyglot kernel for Jupyter.
+ * %%
+ * Copyright (C) 2017 Hadrien Mary
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package org.scijava.plot;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.scijava.notebook.*;
+import com.twosigma.beakerx.mimetype.MIMEContainer;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import net.imagej.notebook.ImageJNotebookService;
+import net.imglib2.RandomAccessibleInterval;
+
+import org.scijava.convert.ConvertService;
+import org.scijava.log.LogService;
+import org.scijava.notebook.converter.output.NotebookOutput;
+import org.scijava.plot.spec.VegaPlot;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+import org.scijava.service.AbstractService;
+import org.scijava.service.Service;
+
+/**
+ * AWT-driven implementation of {@link NotebookService}.
+ *
+ * @author Curtis Rueden
+ * @author Hadrien Mary
+ */
+@Plugin(type = Service.class)
+public class DefaultPlotService extends AbstractService implements
+        PlotService {
+
+    @Override
+    public void writeJson(VegaPlot plot, String filePath) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.writeValue(new File(filePath), plot);
+        } catch (IOException ex) {
+            Logger.getLogger(DefaultPlotService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    public VegaPlot readJson(String filePath) {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            return mapper.readValue(new File(filePath), VegaPlot.class);
+        } catch (IOException ex) {
+            Logger.getLogger(DefaultPlotService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+}
---
+++ src/main/java/org/scijava/plot/PlotService.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2017 SciJava.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.scijava.plot;
+
+import org.scijava.plot.spec.VegaPlot;
+import org.scijava.service.SciJavaService;
+
+/**
+ *
+ * @author hadim
+ */
+public interface PlotService extends SciJavaService {
+    
+    void writeJson(VegaPlot plot, String filePath);
+    VegaPlot readJson(String jsonString);
+    
+}
